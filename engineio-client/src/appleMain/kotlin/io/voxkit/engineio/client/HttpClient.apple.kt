438fd7d6acabf8b33f60aafe01b474dc631be622
Alex Shepeliev <a.shepeliev@gmail.com>
2025-04-11 09:57:27 +0300

    Switch Ktor client engine to CIO
    "Darwin" engine doesn't expose response headers for websocket connection.
    https://youtrack.jetbrains.com/issue/KTOR-6970
    
    Also, it exposes multivalued header as a string instead of a list of strings.
    
    Switched to CIO in Android for consistency.

--- engineio-client/src/appleMain/kotlin/io/voxkit/engineio/client/HttpClient.apple.kt
+++ engineio-client/src/appleMain/kotlin/io/voxkit/engineio/client/HttpClient.apple.kt
@@ -1,10 +1,10 @@
 package io.voxkit.engineio.client
 
 import io.ktor.client.*
-import io.ktor.client.engine.darwin.*
+import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.websocket.*
 
-internal actual fun platformHttpClient(block: HttpClientConfig<*>.() -> Unit): HttpClient = HttpClient(Darwin) {
+internal actual fun platformHttpClient(block: HttpClientConfig<*>.() -> Unit): HttpClient = HttpClient(CIO) {
     apply(block)
     install(WebSockets)
 }
